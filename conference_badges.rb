f620dd29c3c23126c19916ad32670580a86ff86d
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-17 02:47:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array <<  "Hello, my name is #{name}."
+  end 
+  new_array
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
